36ed11e3a97df947ff73953366557eb380249681
Parz007 <pbui29399@gmail.com>
2026-06-19 09:19:09 +1000

    Add redirect from /start to /chat/new

--- artifacts/chat-app/src/App.tsx
+++ artifacts/chat-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route, Router as WouterRouter, Redirect } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -14,6 +14,9 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/start">
+        <Redirect to="/chat/new" />
+      </Route>
       <Route path="/chat/new" component={Chat} />
       <Route path="/chat/:id" component={Chat} />
       <Route path="/settings" component={Settings} />
